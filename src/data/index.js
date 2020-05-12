36f81a4ebb11172c284eac276d5047a1fbe9ac83
Angela Ordonez <angelitaooo@me.com>
2020-05-11 20:20:04 -0500

    fixes in portfolio images

--- src/data/index.js
+++ src/data/index.js
@@ -2,34 +2,34 @@ export default {
   portfolio: [
     {
       title: 'Yuxi Global Website',
-      img: 'yuxi1.png',
+      img: 'yuxi.jpg',
       description: 'This is the website for Yuxi Global in 2018',
       link: 'https://www.yuxiglobal.com/home',
     },
     {
       title: 'Fago Fitness',
-      img: 'fago1.png',
+      img: 'fago.jpg',
       description:
         'Fago is a personal project to help people find healty food.',
       link: '',
     },
     {
       title: 'React Week',
-      img: 'react-week.png',
+      img: 'reactweek.jpg',
       description:
         'This is the Website for React Week 2019 in Globant Medellin.',
       link: 'https://reactweek.org/',
     },
     {
       title: 'Robofriends',
-      img: 'robofriends.png',
+      img: 'robofriends.jpg',
       description:
         'This is a personal project using react.js to fetch some data from a Robots Api.',
       link: 'https://github.com/angelitaooo/robofriends',
     },
     {
       title: 'Smart Brain',
-      img: 'smart-brain.png',
+      img: 'smartbrain.jpg',
       description:
         'This is a personal project using react.js and calling the face detection model with the Clarifai API.',
       link: 'https://github.com/angelitaooo/face-recognition-brain',
